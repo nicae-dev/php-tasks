ffc5ba8854f2bbf27301cccc2fea55ff7802637b
Veronica Efimova <nica.e@yandex.ru>
2026-02-27 18:27:39 +0300

    Дорабатывает классы

--- dekanat/index.php
+++ dekanat/index.php
@@ -13,8 +13,14 @@ class Student {
 class CourseManager {
 
     public array $students = [];
+    public array $courses = [];
 
-    public function getCourses($studentsInfo, $scoresInfo) {
+    public function __construct($studentsInfo, $coursesInfo) {
+        $this->setStudents($studentsInfo);
+        $this->setScores($coursesInfo);
+    }
+
+    public function setStudents($studentsInfo) {
         $studentsInfo = explode(';', $studentsInfo);
         foreach ($studentsInfo as $info) {
             $info = explode(',', $info);
@@ -24,10 +30,39 @@ class CourseManager {
             $this->students[$info[0]]->addCourse($info[1], $info[2]);
         }
     }
+
+    public function setScores($coursesInfo) {
+        $coursesInfo = explode(';', $coursesInfo);
+        foreach ($coursesInfo as $info) {
+            $info = explode(',', $info);
+            $this->courses[$info[0]] = $info[1];
+        }
+    }
+
+    public function coursesWithDuty(): array {
+        $coursesWithoutDuty = $this->courses;
+        foreach ($this->students as $student) {
+            foreach ($student->courses as $st_course => $st_grade) {
+                if ($coursesWithoutDuty[$st_course] && $st_grade < $this->courses[$st_course]) {
+                    unset($coursesWithoutDuty[$st_course]);
+                }
+            }
+        }
+        return $coursesWithoutDuty;
+    }
+    public function coursesWithDutyForOutput(){
+        $coursesWithoutDuty = $this->coursesWithDuty();
+        if ($coursesWithoutDuty) {
+            return implode('\n',$coursesWithoutDuty);
+        } else{
+            return 'Пусто';
+        }
+    }
 }
 
 // ваш код
-$studentsInfo = trim(fgets(STDIN));
-$scoresInfo = trim(fgets(STDIN));
+$studentsInfo = 'Анна,Математика,85;Анна,Химия,90;Борис,Математика,75;Борис,История,80;Евгений,Математика,95';//trim(fgets(STDIN));
+$coursesInfo = 'Математика,80;Химия,60;История,80';// trim(fgets(STDIN));
 
-
+$cm = new CourseManager($studentsInfo, $coursesInfo);
+$coursesWithoutDuty = $cm->coursesWithDuty();
